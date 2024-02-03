58bd16091e3cc63ce8dd99404c63261a8982f105
Tejas Darode <123544186+tejasdarode17@users.noreply.github.com>
2024-02-03 15:55:14 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,5 +1,25 @@
 const boxes = document.querySelectorAll('.box');
 const body = document.querySelector('body');
+const startButton = document.querySelector(".start-button");
+const stopButton = document.querySelector(".stop-button");
+
+
+let id
+startButton.addEventListener('click',(e)=>{
+        id = setInterval(() => {
+                const red = Math.floor(Math.random() * 256);
+                const green = Math.floor(Math.random() * 256);
+                const blue = Math.floor(Math.random() * 256);
+                const rgb = `rgb(${red}, ${green}, ${blue})`;
+                document.body.style.backgroundColor = rgb;
+            }, 10);
+})
+
+stopButton.addEventListener('click',(e)=>{
+    clearInterval(id)
+})
+
+
 
 boxes.forEach(function (button) {
     button.addEventListener('click', function(e){
@@ -7,3 +27,4 @@ boxes.forEach(function (button) {
     })
 
 })
+
